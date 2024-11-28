19823734600652eb2900cab850b84903b7d364c2
Maíra Albuquerque <mairaalbuquerque@fabios-mbp.lan>
2024-11-28 09:44:57 +0000

    Added build

---
+++ build/precache-manifest.afb3fbb7dd83f87df11fe66ea4ca57e0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8e2a0d10c9b248451a8e012899e2dfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e58bb218f71ab2f1eb7",
+    "url": "/static/css/main.5cc9472d.chunk.css"
+  },
+  {
+    "revision": "4bd5b1abd009fccb5144",
+    "url": "/static/js/2.66ebd658.chunk.js"
+  },
+  {
+    "revision": "67ae3e0212c3a537d86a1f099983ebeb",
+    "url": "/static/js/2.66ebd658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "519132f6cdd4ae14261c",
+    "url": "/static/js/3.c5c7afed.chunk.js"
+  },
+  {
+    "revision": "6e58bb218f71ab2f1eb7",
+    "url": "/static/js/main.5bfaf9f3.chunk.js"
+  },
+  {
+    "revision": "2e3b278a14e7043d8c47",
+    "url": "/static/js/runtime-main.f838e2b0.js"
+  },
+  {
+    "revision": "75fc1dd3572b934509b8c574a79a91a5",
+    "url": "/static/media/compartilhar.75fc1dd3.jpg"
+  },
+  {
+    "revision": "eca29a54fe3e91eb3dbcd75cbe2bd3bc",
+    "url": "/static/media/imgchef.eca29a54.jpg"
+  },
+  {
+    "revision": "771aed9c4524a7d1cbac6476f81954da",
+    "url": "/static/media/imgsobre.771aed9c.jpg"
+  },
+  {
+    "revision": "8f58d54a5c7bf1d15f9bc8bd02fca199",
+    "url": "/static/media/prato1.8f58d54a.jpg"
+  },
+  {
+    "revision": "06a7451555ea88d24cdcfaa949c8c949",
+    "url": "/static/media/prato2.06a74515.jpg"
+  },
+  {
+    "revision": "9ba0987d3c51ca2c1252322737d5d6cd",
+    "url": "/static/media/prato3.9ba0987d.jpg"
+  },
+  {
+    "revision": "a91e208d75250a88f66b326e27d5eee2",
+    "url": "/static/media/prato4.a91e208d.jpg"
+  },
+  {
+    "revision": "27ff0d267b86dbf8f7ad55ccaa27e893",
+    "url": "/static/media/prato5.27ff0d26.jpg"
+  },
+  {
+    "revision": "120e0c567b13cd63901b32ac52e2405c",
+    "url": "/static/media/prato6.120e0c56.jpg"
+  },
+  {
+    "revision": "172d5247d3a933b5ba66e10857fe4cad",
+    "url": "/static/media/prato7.172d5247.jpg"
+  },
+  {
+    "revision": "9e162f10e34f43573da29e2f2cbf227d",
+    "url": "/static/media/prato8.9e162f10.jpg"
+  },
+  {
+    "revision": "8994196c6bbc6804a80155ab84401568",
+    "url": "/static/media/reservas.8994196c.jpg"
+  },
+  {
+    "revision": "c758df6bf128dd6d325f4a97b9c916a3",
+    "url": "/static/media/restinicial.c758df6b.jpg"
+  }
+]);
