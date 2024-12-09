d6c060a47dea9b95e67d5ff2849a18cbbe708aa3
Maíra Albuquerque <mairaalbuquerque@fabios-mbp.lan>
2024-12-09 14:33:27 +0000

    Deploy to GitHub Pages

---
+++ precache-manifest.8d2e6448ce7e0c3b0112238e08b7c5fc.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2303c9a926d5d5adfeea10931c1bbbf7",
+    "url": "/Projeto-Final-Tokio-School/index.html"
+  },
+  {
+    "revision": "74b8ac03106db2d9661a",
+    "url": "/Projeto-Final-Tokio-School/static/css/main.3df72bdf.chunk.css"
+  },
+  {
+    "revision": "4bd5b1abd009fccb5144",
+    "url": "/Projeto-Final-Tokio-School/static/js/2.66ebd658.chunk.js"
+  },
+  {
+    "revision": "67ae3e0212c3a537d86a1f099983ebeb",
+    "url": "/Projeto-Final-Tokio-School/static/js/2.66ebd658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "519132f6cdd4ae14261c",
+    "url": "/Projeto-Final-Tokio-School/static/js/3.c5c7afed.chunk.js"
+  },
+  {
+    "revision": "74b8ac03106db2d9661a",
+    "url": "/Projeto-Final-Tokio-School/static/js/main.8dcafdce.chunk.js"
+  },
+  {
+    "revision": "e157e92f40d604140544",
+    "url": "/Projeto-Final-Tokio-School/static/js/runtime-main.b882d54a.js"
+  },
+  {
+    "revision": "75fc1dd3572b934509b8c574a79a91a5",
+    "url": "/Projeto-Final-Tokio-School/static/media/compartilhar.75fc1dd3.jpg"
+  },
+  {
+    "revision": "eca29a54fe3e91eb3dbcd75cbe2bd3bc",
+    "url": "/Projeto-Final-Tokio-School/static/media/imgchef.eca29a54.jpg"
+  },
+  {
+    "revision": "771aed9c4524a7d1cbac6476f81954da",
+    "url": "/Projeto-Final-Tokio-School/static/media/imgsobre.771aed9c.jpg"
+  },
+  {
+    "revision": "8f58d54a5c7bf1d15f9bc8bd02fca199",
+    "url": "/Projeto-Final-Tokio-School/static/media/prato1.8f58d54a.jpg"
+  },
+  {
+    "revision": "06a7451555ea88d24cdcfaa949c8c949",
+    "url": "/Projeto-Final-Tokio-School/static/media/prato2.06a74515.jpg"
+  },
+  {
+    "revision": "9ba0987d3c51ca2c1252322737d5d6cd",
+    "url": "/Projeto-Final-Tokio-School/static/media/prato3.9ba0987d.jpg"
+  },
+  {
+    "revision": "a91e208d75250a88f66b326e27d5eee2",
+    "url": "/Projeto-Final-Tokio-School/static/media/prato4.a91e208d.jpg"
+  },
+  {
+    "revision": "27ff0d267b86dbf8f7ad55ccaa27e893",
+    "url": "/Projeto-Final-Tokio-School/static/media/prato5.27ff0d26.jpg"
+  },
+  {
+    "revision": "120e0c567b13cd63901b32ac52e2405c",
+    "url": "/Projeto-Final-Tokio-School/static/media/prato6.120e0c56.jpg"
+  },
+  {
+    "revision": "172d5247d3a933b5ba66e10857fe4cad",
+    "url": "/Projeto-Final-Tokio-School/static/media/prato7.172d5247.jpg"
+  },
+  {
+    "revision": "9e162f10e34f43573da29e2f2cbf227d",
+    "url": "/Projeto-Final-Tokio-School/static/media/prato8.9e162f10.jpg"
+  },
+  {
+    "revision": "8994196c6bbc6804a80155ab84401568",
+    "url": "/Projeto-Final-Tokio-School/static/media/reservas.8994196c.jpg"
+  },
+  {
+    "revision": "c758df6bf128dd6d325f4a97b9c916a3",
+    "url": "/Projeto-Final-Tokio-School/static/media/restinicial.c758df6b.jpg"
+  }
+]);
